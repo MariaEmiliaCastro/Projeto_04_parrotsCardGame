270abd3d8532be98950c1f8fcce4dce73e4405cd
MariaEmiliaCastro <emyly.mary@gmail.com>
2022-04-09 11:18:55 -0300

    Lógica de distribuição das cartas implementadas

--- js/main.js
+++ js/main.js
@@ -3,28 +3,54 @@ function comparador() {
 }
 
 function popularArray (quantidade) {
-    let arrayCartas = ["img/bobrossparrot.gif", "img/explodyparrot.gif", "img/fiestaparrot.gif", "img/metalparrot.gif", "img/vertitparrot.gif", "img/tripletsparrot.gif", "img/unicornparrot.gif"];
+    let arrayCartas = ["img/bobrossparrot.gif", "img/explodyparrot.gif", "img/fiestaparrot.gif", "img/metalparrot.gif", "img/revertitparrot.gif", "img/tripletsparrot.gif", "img/unicornparrot.gif"];
     console.log(arrayCartas);
     arrayCartas.sort(comparador);
     console.log("After the change: " + arrayCartas);
 
     let arrayAtual = [];
 
-    for(let i = 0; i < quantidade; i++) {
+    for(let i = 0; i < (quantidade/2); i++) {
         arrayAtual.push(arrayCartas[i]);
     }
+
+    for(let i = 0; i < (quantidade/2); i++) {
+        arrayAtual.push(arrayCartas[i]);
+    }
+
+    arrayAtual.sort(comparador);
 
     return arrayAtual;
 } 
 
+function distribuirCartas(quantidade){
+    let arrayCartas = popularArray(quantidade);
+    let table = document.querySelector(".table");
 
-
-
-
-let numCartas = prompt("Deseja jogar com quantas cartas?");
-
-while(numCartas % 2 !== 0 || numCartas < 4 || numCartas > 14){
-    numCartas = prompt("Deseja jogar com quantas cartas?");
+    for(let i = 0; i < quantidade; i++) {
+        table.innerHTML += `
+        <div class="card" onclick="virarDesvirar(this)">
+            <div class="face">
+                <img src="img/front.png" alt="">
+            </div>
+            <div class="hidden">
+                <img src=${arrayCartas[i]} alt="">
+            </div>
+        </div>
+        `
+    }
 }
+
+function virarDesvirar(carta) {
+    console.log("Clicooooou!")
+}
+
+let numCartas = 14;
+
+//let numCartas = prompt("Deseja jogar com quantas cartas?");
+
+// while(numCartas % 2 !== 0 || numCartas < 4 || numCartas > 14){
+//     numCartas = prompt("Deseja jogar com quantas cartas?");
+// }
 
 distribuirCartas(numCartas);
